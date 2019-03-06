dc839428ba2e778a4fb76b37572d6805ef9ee842
Code dạo kiếm cơm <zinzinx8@gmail.com>
2019-03-06 15:20:05 +0700

    Update Module.php

--- src/Module.php
+++ src/Module.php
@@ -1,2 +1,45 @@
 <?php
+namespace izi\dashboad;
 
+use Yii;
+
+class Module extends \yii\base\Module
+
+{
+    public $moduleName = 'dashboad';
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace ;
+    
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        Yii::setAlias('@dashboad', __DIR__);
+        
+        $this->controllerNamespace = "izi\\dashboad\\controllers";
+        
+        parent::init();
+        Yii::$app->view->theme = new \izi\web\Theme([
+            'pathMap' => [
+                /**
+                 * thiết lập pathmap cho các mục cấp con trước (thư mục có độ sâu lớn nhất phải thiết lập đầu tiên)
+                 *     
+                 ### uncomment if you want
+                '@dashboad' => [
+                    "@app/modules/dashboads/v1.5",
+                    "@app/modules/dashboads/v1.0",
+                ],
+                */
+            ],
+            'baseUrl' => "@web/themes/{$this->moduleName}",
+            'basePath'=>'@dashboad/views',
+            'viewPath'=>'@dashboad/views'
+        
+        ]);
+        // custom initialization code goes here
+
+    }
+}
